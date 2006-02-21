d4f8eed2e8b35e64c1b6e06cbcc4024790a67c71
fergal@dyn.fergaldaly.com <>
2006-02-21 23:10:52 +0100

    more stuff for setup.py

--- setup.py
+++ setup.py
@@ -3,4 +3,8 @@ from distutils.core import setup
 setup(name='deep',
       version='0.1',
       py_modules=['deep'],
+      author="Fergal Daly",
+      author_email="fergal@esatclear.ie",
+      description="Easy, flexible deep comparison and testing of structured data",
+      download_url="http://www.fergaldaly.com/computer/python/deep",
       )
