90aaf3f72ebabd187a4c17243471675d47aa49cd
Fergal Daly <fergal@dyn.fergaldaly.com>
2008-06-06 13:12:00 +0100

    update license

--- setup.py
+++ setup.py
@@ -10,4 +10,5 @@ setup(name='deep',
       description="Easy, flexible deep comparison and testing of structured data",
       url="http://code.google.com/p/python-deep/",
       download_url="http://code.google.com/p/python-deep/downloads/list",
+      license="LGPL",
       )
