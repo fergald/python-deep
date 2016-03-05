78ae4fe8690bbdab5687e6fcfc9862e0af1cd6c5
Fergal Daly <fergal@esatclear.ie>
2016-03-05 05:46:17 +0000

    update version

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@
 from distutils.core import setup
 
 setup(name='deep',
-      version='0.9',
+      version='0.10',
       packages = ['deep'],
       author="Fergal Daly",
-      author_email="fergal@esatclear.ie",
+      author_email="fergald@gmail.com",
       description="Easy, flexible deep comparison and testing of structured data",
       url="http://code.google.com/p/python-deep/",
       download_url="http://code.google.com/p/python-deep/downloads/list",
